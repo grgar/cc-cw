88900f8e1dab206c9e1ecb22dfcf5704ce519551
George Garside <grg.garside@gmail.com>
2018-10-27 22:01:50 +0100

    Replace TextArrayWritable with SetWritable, removing array intermediary

--- src/Anagrams.kt
+++ src/Anagrams.kt
@@ -3,9 +3,9 @@ package com.georgegarside.coc105.anagrams
 import org.apache.hadoop.conf.Configured
 import org.apache.hadoop.fs.FileSystem
 import org.apache.hadoop.fs.Path
-import org.apache.hadoop.io.ArrayWritable
 import org.apache.hadoop.io.LongWritable
 import org.apache.hadoop.io.Text
+import org.apache.hadoop.io.Writable
 import org.apache.hadoop.mapreduce.*
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat
@@ -13,15 +13,16 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat
 import org.apache.hadoop.util.Tool
 import org.apache.hadoop.util.ToolRunner
+import java.io.DataInput
+import java.io.DataOutput
 import java.io.DataOutputStream
 import java.util.*
 import kotlin.system.exitProcess
 
 class Anagrams : Configured(), Tool {
-	class AnagramMapper : Mapper<LongWritable, Text, Text, TextArrayWritable>() {
+	class AnagramMapper : Mapper<LongWritable, Text, Text, SetWritable>() {
 		private val keyOut = Text()
-		private val valOut = TextArrayWritable()
-		private val valString = Text()
+		private val valOut = SetWritable()
 		override fun map(key: LongWritable, value: Text, context: Context) = value
 				.toString()
 				.split("[^\\w'’-]|--|_".toRegex())
@@ -30,25 +31,40 @@ class Anagrams : Configured(), Tool {
 				.map { it.toLowerCase(Locale.getDefault()) }
 				.forEach { word ->
 					val chars = word.toCharArray().sortedArray().filterNot { c -> c == '-' || c == '\'' || c == '’' }.joinToString(separator = "")
-					context.write(keyOut.apply { set(chars) }, valOut.apply { set(arrayOf(valString.apply { set(word) })) })
+					context.write(keyOut.apply { set(chars) }, valOut.apply { values = setOf(word) })
 				}
 	}
 
-	class AnagramReducer : Reducer<Text, TextArrayWritable, Text, TextArrayWritable>() {
-		override fun reduce(key: Text, values: MutableIterable<TextArrayWritable>, context: Context) {
+	class AnagramReducer : Reducer<Text, SetWritable, Text, SetWritable>() {
+		override fun reduce(key: Text, values: MutableIterable<SetWritable>, context: Context) {
 			context.write(key, values.reduce { acc, cur ->
-				val list = (acc.toStrings() + cur.toStrings()).toSortedSet().toTypedArray()
-				TextArrayWritable(list)
+				val vals = (acc.values + cur.values)
+				SetWritable(vals)
 			})
 		}
 	}
 
-	class TextArrayWritable : ArrayWritable {
-		constructor() : super(Text::class.java)
-		constructor(array: Array<String>) : super(array)
-		constructor(string: String) : super(arrayOf(string))
+	class SetWritable(var values: Set<String>) : Writable {
+		constructor() : this(setOf())
 
-		override fun toString() = toStrings()?.contentDeepToString() ?: ""
+		override fun readFields(input: DataInput) {
+			val size = input.readInt()
+			values = (0 until size).fold(setOf()) { acc, _ ->
+				acc + input.readUTF()
+			}
+		}
+
+		override fun write(output: DataOutput) {
+			output.writeInt(values.size)
+			values.forEach { output.writeUTF(it) }
+		}
+
+		operator fun plus(other: SetWritable): SetWritable {
+			values += other.values
+			return this
+		}
+
+		override fun toString() = values.joinToString(prefix = "{ ", separator = ", ", postfix = " }")
 	}
 
 	override fun run(vararg args: String) =
@@ -77,7 +93,7 @@ class Anagrams : Configured(), Tool {
 						inputFormatClass = TextInputFormat::class.java
 
 						outputKeyClass = Text::class.java
-						outputValueClass = TextArrayWritable::class.java
+						outputValueClass = SetWritable::class.java
 
 						outputFormatClass = AnagramOutput::class.java
 					}
@@ -98,14 +114,13 @@ class Anagrams : Configured(), Tool {
 	companion object {
 		class AnagramOutput<K, V> : TextOutputFormat<K, V>() {
 			override fun getRecordWriter(job: TaskAttemptContext): RecordWriter<K, V> =
-					AnagramLineOutput(getDefaultWorkFile(job, "txt").let {
+					AnagramLineOutput(getDefaultWorkFile(job, ".txt").let {
 						it.getFileSystem(job.configuration).create(it, true)
 					})
 
 			private inner class AnagramLineOutput(out: DataOutputStream) : LineRecordWriter<K, V>(out, "") {
 				override fun write(key: K, value: V) {
-					// .get() outperforms .toStrings() and .toArray(), per implementation operations
-					if (value is ArrayWritable && value.get().size == 1) return
+					if (value is SetWritable && value.values.size == 1) return
 					super.write(null, value)
 				}
 			}
